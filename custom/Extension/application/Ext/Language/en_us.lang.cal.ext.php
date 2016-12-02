0c2d6d0f58f946978b00e4d2979957e89fd67a51
Angel Magaña <cheleguanaco@cheleguanaco.com>
2016-12-01 17:13:18 -0800

    Create en_us.lang.cal.ext.php

---
+++ custom/Extension/application/Ext/Language/en_us.lang.cal.ext.php
@@ -0,0 +1,8 @@
+<?php
+
+/* File: ./custom/Extension/application/Ext/Language/en_us.lang.cal.ext.php */
+
+$app_strings['LBL_CAL_DASHLET'] = 'My Graphical Calendar';
+$app_strings['LBL_CAL_DASHLET_DESC'] = 'Graphical calendar dashlet for current user';
+
+?>
